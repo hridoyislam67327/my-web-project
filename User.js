55b2ef656c5122558145eeb5b4f18751fb5d0204
hridoyislam67327 <hridoyislam67327@gmail.com>
2026-09-16 02:06:03 -0700

    Update and rename User.js to models/User.js.

--- User.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-module.exports = mongoose.model('User', new mongoose.Schema({
-  telegramId: String, username: String, firstName: String, balance: { type: Number, default: 0 }, isSuspended: { type: Boolean, default: false }
-}));
