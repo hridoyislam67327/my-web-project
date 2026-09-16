b1f1cf1e879732c195f6eaab13cb2949e9869168
hridoyislam67327 <hridoyislam67327@gmail.com>
2026-09-16 10:42:38 -0700

    Delete models directory

--- models/NumberRange.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const numberRangeSchema = new mongoose.Schema({
-  country: { type: String, required: true },
-  prefix: { type: String, required: true },
-  ranges: [String],
-  status: { type: String, default: 'Active' }
-});
-module.exports = mongoose.model('NumberRange', numberRangeSchema);
--- models/Settings.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const settingsSchema = new mongoose.Schema({
-  otpRate: { type: Number, default: 1.0 },
-  channelLink: { type: String, default: 'https://t.me/your_channel' },
-  topMessageText: { type: String, default: 'Welcome to OTP Bot. Select your options below:' }
-});
-module.exports = mongoose.model('Settings', settingsSchema);
