0d7d509d9de9201ee1cccdf299e41ae8f4eb570d
hridoyislam67327 <hridoyislam67327@gmail.com>
2026-09-16 01:57:39 -0700

    Create User.js

---
+++ User.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+module.exports = mongoose.model('User', new mongoose.Schema({
+  telegramId: String, username: String, firstName: String, balance: { type: Number, default: 0 }, isSuspended: { type: Boolean, default: false }
+}));
