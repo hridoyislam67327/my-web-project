dbcb8beebe54973200a20e22da625a7aaf802de1
hridoyislam67327 <hridoyislam67327@gmail.com>
2026-09-16 04:52:24 -0700

    Remove duplicate express app initialization

--- bot.js
+++ bot.js
@@ -190,7 +190,3 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Bot is active!'));
 app.listen(process.env.PORT || 3000);
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Bot is running...'));
-app.listen(process.env.PORT || 3000);
